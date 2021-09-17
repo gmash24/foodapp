85a3eabb9cf71ac67b15773f8d1ce955f5cec336
Gift Masha <gftman24@gmail.com>
2021-09-17 14:20:17 +0300

    changed background color modified adapter

--- app/src/main/java/com/e/myapp/MainActivity.java
+++ app/src/main/java/com/e/myapp/MainActivity.java
@@ -82,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager =new LinearLayoutManager(this,RecyclerView.VERTICAL,false);
         ar.setLayoutManager(layoutManager);
         Availableadapter =new availableadapter(this,popularrecycler);
-        pr.setAdapter(Availableadapter);
+        ar.setAdapter(Availableadapter);
 
     }
 }
